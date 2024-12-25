ec2d074a1e8dbc262f17df00b227469d6daee6f9
Gavin Cao <gavincao2017@hotmail.com>
2024-12-25 16:08:12 +1300

    Fix deployment issues
    Attempt 3

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -13,19 +13,25 @@ export default async function Page() {
 
   return (
     <div className="w-full">
-      {session?.user?.role !== "admin" ? (
+      {session?.user?.role ? (
+        session?.user?.role !== "admin" ? (
+          <div className="flex w-full items-center justify-between">
+            You do not have access to this page.
+          </div>
+        ) : (
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <h1 className={`${lusitana.className} text-2xl`}>Admin</h1>
+            </div>
+            <div className="md:mt-8">Create User</div>
+            <UserForm />
+            <div className="md:mt-8">Edit Users</div>
+            <Table otherUsers={otherUsers} />
+          </div>
+        )
+      ) : (
         <div className="flex w-full items-center justify-between">
           You do not have access to this page.
-        </div>
-      ) : (
-        <div>
-          <div className="flex w-full items-center justify-between">
-            <h1 className={`${lusitana.className} text-2xl`}>Admin</h1>
-          </div>
-          <div className="md:mt-8">Create User</div>
-          <UserForm />
-          <div className="md:mt-8">Edit Users</div>
-          <Table otherUsers={otherUsers} />
         </div>
       )}
     </div>
